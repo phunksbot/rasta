04c5420406647b211c27558c0a892445a13c3191
phunksbot <86929652+phunksbot@users.noreply.github.com>
2021-08-31 16:36:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function formatAndSendTweet(event) {
     const formattedEthPrice = ethers.utils.formatEther(totalPrice.toString());
     const formattedUsdPrice = (formattedEthPrice * usdValue).toFixed(2);
 
-    const tweetText = `${tokenName} was flipped for Ξ${formattedEthPrice} ($${formattedUsdPrice})\n ${openseaLink}\n #CryptoPhunks #Phunks #AltPhunks`;
+    const tweetText = `${tokenName} was purchased for Ξ${formattedEthPrice} ($${formattedUsdPrice})\n ${openseaLink}\n #cryptorastas #nfts #altpunks`;
 
     console.log(tweetText);
 
